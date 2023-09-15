c865242d202062ffa72dc25088dadfef75f0f61a
clodal <joel@onextech.com>
2023-09-15 23:06:40 +0800

    docs: logging response to debug on prod

--- app/(pages)/areas/page.tsx
+++ app/(pages)/areas/page.tsx
@@ -5,6 +5,7 @@ export const dynamic = 'force-dynamic'
 
 const Areas = async () => {
     const res = await fetch(`${process.env.API_BASE_URL}/areas`, { cache: 'no-store' });
+    console.log('[DEBUG on Prod]: res', res)
     const areas = res.status === 200 ? await res.json() : []
 
     return (
