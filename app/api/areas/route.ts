64b283283adb6c2f679fd21ee44f4d55ff4f62eb
clodal <joel@onextech.com>
2023-09-15 23:18:23 +0800

    gc docs: add debug info to debug on prod

--- app/api/areas/route.ts
+++ app/api/areas/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export const GET = async (request: NextRequest) => {
+    console.log('[DEBUG on Prod API]: Init')
     try {
         const areas = await prisma.area.findMany({
             orderBy: {
@@ -19,11 +20,13 @@ export const GET = async (request: NextRequest) => {
 
         })
         if (!areas.length) {
+          console.log('[DEBUG on Prod API]: areas not found')
             return NextResponse.json({ message: "No areas found in DB" }, { status: 404 })
         }
       console.log('[DEBUG on Prod API]: areas', areas)
         return NextResponse.json(areas, { status: 200 })
     } catch (error) {
+      console.log('[DEBUG on Prod API]: err', error)
         return NextResponse.json({ message: "Error Getting Areas", error }, { status: 500 })
     }
 }
