a6c1ccd85545919887fbee1432887318d5d1ab3c
Jamshid Ghiasinejad <jamshidghiasinejad@jamshidcstudio7.lan>
2023-09-23 00:53:55 +1000

    Added loading to category

---
+++ app/(pages)/categories/loading.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/layout/layout"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Loader2 } from "lucide-react"
+const CategoriesPageLoading = () => {
+  return (
+    <Layout>
+      <div className="flex flex-col w-full items-center justify-center   h-[50vh] max-h-full">
+
+        Loading Categories...
+        <Loader2 className="mr-2 h-24 w-24 animate-spin" />
+      </div>
+    </Layout>
+  )
+}
+export default CategoriesPageLoading
