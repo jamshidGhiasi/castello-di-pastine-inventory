5a477c2fd86b391ceab43112de76148e9c5919f6
Jamshid Ghiasinejad <jamshidghiasinejad@jamshidcstudio7.lan>
2023-09-24 21:24:45 +1000

    Trying new css for scroll containers

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import HomeNavigation from '@/components/home-navigations'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6 z-10 relative">
+    <main className="flex max-h-screen flex-col items-center justify-between p-6 z-10 relative">
 
 
       <div className="relative flex grow">
--- components/area-item.tsx
+++ components/area-item.tsx
@@ -10,16 +10,11 @@ export const AreaItem = ({ title, slug, count }: { title: string, slug: string, 
     return (
         <Button className="group w-full py-4 sm:py-4 flex justify-between  items-center rounded-full  h-auto bg-white/40" onClick={handleAreaItemClick}>
             <div className="h-12 w-12 sm:w-12 sm:h-12 flex items-center justify-center rounded-full mr-4 bg-white group-hover:bg-[#a8c2b8]">
-                <Castle className="h-4 w-4 sm:h-auto sm:w-auto" />
+            {count}
             </div>
-            <div className="flex flex-col items-start justify-center mr-auto">
-                <span className=" sm:text-lg font-bold">
+            <span className="text-lg font-bold">
                     {title.toUpperCase()}
                 </span>
-                <span>
-                    {count}
-                </span>
-            </div>
             <div className="h-12 w-12 sm:w-12 sm:h-12 flex items-center justify-center rounded-full  bg-[#111815] text-white ml-auto group-hover:text-[#a8c2b8]">
                 <ChevronRight />
             </div>
