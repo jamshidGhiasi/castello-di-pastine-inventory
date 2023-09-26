640771414784f5e3f3c0604b52f395032800ecbc
Jamshid Ghiasinejad <jamshidghiasinejad@jamshidcstudio7.lan>
2023-09-26 14:08:28 +1000

    Style updates for room-item

--- components/room-item.tsx
+++ components/room-item.tsx
@@ -8,18 +8,18 @@ export const RoomItem = ({ title, areaId, slug, count, roomNumber }: { title: st
         router.push(`/areas/${areaId}/${slug}`)
     }
     return (
-        <Button className="group w-full bg-transparent text-black flex justify-between   items-center  py-6 px-2 bg-white shadow-sm rounded-full" onClick={handleAreaItemClick}>
+        <Button className="group w-full  text-black flex justify-between border   items-center  py-8 px-2 bg-white shadow-sm rounded-full hover:bg-[#ebf1ef] hover:shadow-lg  transition-all duration-400 ease-out hover:border hover:border-[#c4d5ce]" onClick={handleAreaItemClick}>
             
 
-                <div className="mr-2 p-2  rounded-full w-9 h-9 flex items-center justify-center"><span className=" text-neutral-400">{count}</span></div>
+            <div className="mr-2 p-2  rounded-full w-9 h-9 flex items-center justify-center bg-[#dadada] text-black mx-2 text-xs group-hover:text-white group-hover:bg-black transition-all duration-400 ease-out "><span className="text-xs" >{count}</span></div>
             
       
-                <div className="flex flex-col">
-                   <p className="text-md">{title}</p>
-                   
-                </div>
-                <div className="flex items-center justify-center justify-self-end   ml-auto  text-black">
-                <ChevronRight />
+            <div className="flex flex-col">
+                <p className="text-md text-left">{title}</p>
+
+            </div>
+                <div className="flex items-center justify-center justify-self-end  rounded-full   ml-auto mr-2 text-[#aaaaaa]  ">
+                <ChevronRight className="w-8 h-8" />
             </div>
         </Button>
     )
