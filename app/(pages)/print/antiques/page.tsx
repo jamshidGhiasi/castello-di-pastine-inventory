a950800a987d801f621b6d6d6dc40d5b441a82e4
Jamshid Ghiasinejad <jamshidghiasinejad@jamshidcstudio7.lan>
2023-09-30 00:25:03 +1000

    Updates to print page

--- app/(pages)/print/antiques/page.tsx
+++ app/(pages)/print/antiques/page.tsx
@@ -6,68 +6,23 @@ import { Input } from "@/components/ui/input";
 import { Img } from "react-image";
 import ReactToPrint from 'react-to-print';
 import QRCode from "react-qr-code";
+import { Button } from "@/components/ui/button";
+import toast from 'react-hot-toast'
+import fetchAntiquesByRange from "@/utils/fetchAntiquesByRange";
+
+
 
 const PrintAntiques = () => {
-    const [antiques, setAntiques] = useState<any[]>([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState<any>(null)
-    const [range, setRange] = useState<string>('')
     const componentRef = useRef(null);
-
-    function getAntiques() {
-
-        const regex = new RegExp(/^\d+(-\d+)?(?:,\d+(?:-\d+)?)+$/)
-        const cleanInput = range.replace(/ /g, '');
-        if (!regex.test(cleanInput)) {
-            setError("Invalid input")
-            return null
-        }
-
-        setLoading(true)
-        setError(null)
-
-        fetch(`${process.env.API_BASE_URL}/search?p=${convertInputToAntiqueRange(range).join(',')}`)
-            .then(res => res.json())
-            .then(data => {
-                setAntiques(data)
-                console.log(data)
-                setLoading(false)
-            })
-            .catch(err => {
-                setLoading(false)
-                setError(err)
-                console.log(err)
-            })
-    }
-
-    function onSearch(event: React.FormEvent) {
-        event.preventDefault()
-        setError(null)
-        getAntiques()
-    }
+    const [antiques, setAntiques] = useState<any[]>([])
     return (
         <Layout>
-            <div className='sticky top-[79px] bg-[#f2f2f2/80] backdrop-blur-sm  border-b py-2 px-4 mb-4 w-full flex items-center justify-between'>
-                <h1 className='font-bold sm:text-lg '>Select an range</h1>
+            <div className='sticky top-[79px] bg-[#f2f2f2/80] backdrop-blur-sm  border-b py-2 px-4 mb-4 w-full flex items-center justify-start'>
+                <h1 className='font-bold sm:text-lg '>Enter an range</h1>
             </div>
-            {range}
-            <form
-                className="flex justify-center items-center w-full sm:w-1/2 mb-8"
-                onSubmit={onSearch}
-            >
-                <Input
-                    type="text"
-                    className="px-5 py-1 w-2/3 sm:px-5 sm:py-5 text-zinc-200 bg-zing-800 rounded-full"
-                    placeholder="Search"
-                    value={range}
-                    onChange={(e) => setRange(e.target.value)}
-                    onInput={() => setError(null)}
-                />
-            </form>
 
-            <button onClick={() => getAntiques()}>New</button>
-            {loading && <div>Loading...</div>}
-            {error !== null && <div>{error}</div>}
+
+
             <ReactToPrint
                 trigger={() => <button>Print this out!</button>}
                 content={() => componentRef.current}
@@ -75,7 +30,7 @@ const PrintAntiques = () => {
             <div ref={componentRef}>
 
 
-                {!loading && antiques && antiques.length && antiques.map((antique, index) => (
+                {/* {antiques && antiques.length && antiques.map((antique, index) => (
                     <div key={index} className="page-a4">
                         <Img src={`/antiques/image${antique?.itemNo.replace('0', '')}.png`} />
                         <h3>ITEM NO</h3>
@@ -89,7 +44,7 @@ const PrintAntiques = () => {
                         <p className="font-bold mb-8">QR Code</p>
                         <QRCode value={`https://castello-di-pastine.com/${antique.itemNo}-2`} />
                     </div>
-                ))}
+                ))} */}
             </div>
         </Layout>
     );
