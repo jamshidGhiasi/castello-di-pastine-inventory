b76fbd03ea670cb24659b49946874a610057a6ff
Jamshid Ghiasinejad <jamshidghiasinejad@jamshidcstudio7.lan>
2023-10-11 21:35:57 +1100

    d

--- app/(pages)/areas/[areaId]/page.tsx
+++ app/(pages)/areas/[areaId]/page.tsx
@@ -22,12 +22,12 @@ const Area = async ({ params }: { params: { areaId: string } }) => {
                     <Link href='/areas' className=' capitalize pointer-events-none' >{areaId.replace(/-/g, ' ')}</Link>
                 </div>
             </div>
-            <div className='sticky  sm:static top-[79px] bg-[#f2f2f2]  border-b py-2 pt-2 px-4 mb-4 w-full max-w-5xl mx-auto   flex items-center justify-between z-20 '>
+            <div className='sticky  sm:static top-[79px] bg-[#f2f2f2]  border-b py-2 pt-2 px-4 mb-4 w-full max-w-5xl mx-auto flex items-center justify-between z-20 '>
                 <h1 className='font-bold sm:text-lg '>Select a room</h1>
                 <SelectDemo areaId={areaId as string} />
             </div>
             {!area && <div>oops! It looks like there are not data for {areaId} yet. </div>}
-            <div className=' grid grid-cols-1 lg:grid lg:grid-cols-3 place-items-center place-content-center gap-3  w-full max-w-5xl mx-auto  px-4 pt-0'>
+            <div className=' grid grid-cols-1 lg:grid lg:grid-cols-3 place-items-center place-content-center gap-3  w-full max-w-5xl mx-auto  px-4 pt-0 sm:pb-36'>
                 {area && area.rooms.map((room: any) => (
                     <RoomItem key={room.id} title={room.title} slug={room.slug} count={room._count.antiques} isDisabled={(room._count.antiques == '0') ? true : false} areaId={areaId} roomNumber={room.title.match(/^(\d+).*/)?.[1]} />
                 ))}
